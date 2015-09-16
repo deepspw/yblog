f0388bc5333b16f5b1105f774b812d836d7bc30b
ItsMeTomB <tomball.me@icloud.com>
2015-09-16 08:07:57 +0100

    Redid navbar to fit dotted style

--- blog.py
+++ blog.py
@@ -8,10 +8,37 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
 	title = "Waiyin Lau - Rigger/Character TD"
-	paragraph = ["Line 1", "Line 2"]
-	return render_template("index.html", title = title , paragraph = paragraph)
+	posttitle = "Insert Post Title Here"
+	postcontent = "paragraph of content here"
+	return render_template("index.html", title = title , posttitle = posttitle, postcontent = postcontent)
 
+@app.route('/reel/')
+def reel():
+	return render_template('index.html')
 
+@app.route('/scripts/')
+def scripts():
+	return render_template('index.html')
+
+@app.route('/about/')
+def about():
+	return render_template('index.html')
+
+@app.route('/cv/')
+def cv():
+	return render_template('index.html')
+
+@app.route('/contact/')
+def contact():
+	return render_template('index.html')
+
+@app.route('/blog/')
+def blog():
+	return render_template('index.html')
+
+@app.route('/doodles/')
+def doodles():
+	return render_template('index.html')
 
 if __name__ == "__main__":
 	app.run(debug=True)
